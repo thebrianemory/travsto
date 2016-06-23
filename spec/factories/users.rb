52f701271e39c87035abfe5ee819d7560d69206b
thebrianemory <the@brianemory.com>
2016-06-22 20:43:04 -0700

    DRY up the user tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    first_name 'Jane'
-    last_name 'Doe'
-    username 'janedoe86'
-    email 'janedoe@example.com'
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    username { "#{last_name}123" }
+    email { "#{first_name}#{last_name}@example.com" }
     password 'password'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,57 +6,54 @@ RSpec.describe User, type: :model do
     expect(build(:user)).to be_valid
   end
 
-  before :each do
-    @first_user = User.create(first_name: 'John', last_name: 'Doe', username: 'johndoe86', email: 'johndoe@example.com', password: 'password')
-  end
-
   describe "misc" do
     it "is valid with a first name, last name, username, and email" do
-      expect(@first_user).to be_valid
+      expect(build(:user)).to be_valid
     end
     it "returns a user's full name as a string" do
-      expect(@first_user.name). to eq 'John Doe'
+      user = build(:user, first_name: 'John', last_name: 'Doe')
+      expect(user.name).to eq 'John Doe'
     end
   end
 
   describe "has a valid username length" do
     it "has a username between 6 and 15 characters" do
-      @first_user.username = 'bobbyjohnson'
-      expect(@first_user).to be_valid
+      user = build(:user, username: 'bobbyjohnson')
+      expect(user).to be_valid
     end
     it "cannot have a username less than 6 characters" do
-      @first_user.username = 'bobby'
-      expect(@first_user).to_not be_valid
+      user = build(:user, username: 'bobby')
+      expect(user).to_not be_valid
     end
     it "it cannot have a username more than 15 characters" do
-      @first_user.username = 'bobbyjohnson1234'
-      expect(@first_user).to_not be_valid
+      user = build(:user, username: 'bobbyjohnson1234')
+      expect(user).to_not be_valid
     end
   end
 
   describe "information cannot be left blank" do
     it "is invalid without a first name" do
-      user = User.new(first_name: nil)
+      user = build(:user, first_name: nil)
       user.valid?
       expect(user.errors[:first_name]).to include("can't be blank")
     end
     it "is invalid without a last name" do
-      user = User.new(last_name: nil)
+      user = build(:user, last_name: nil)
       user.valid?
       expect(user.errors[:last_name]).to include("can't be blank")
     end
     it "is invalid without an username" do
-      user = User.new(username: nil)
+      user = build(:user, username: nil)
       user.valid?
       expect(user.errors[:username]).to include("can't be blank")
     end
     it "is invalid without an email address" do
-      user = User.new(email: nil)
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
     it "is invalid without a password" do
-      user = User.new(password: nil)
+      user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
@@ -64,14 +61,16 @@ RSpec.describe User, type: :model do
 
   describe "information cannot already be in use" do
     it "is invalid with a duplicate email address" do
-      user = User.new(first_name: 'John', last_name: 'Doe', username: 'johndoe88', email: 'johndoe@example.com', password: 'password')
-      user.valid?
-      expect(user.errors[:email]).to include("has already been taken")
+      user1 = create(:user, email: 'duplicate@example.com')
+      user2 = build(:user, email: 'duplicate@example.com')
+      user2.valid?
+      expect(user2.errors[:email]).to include("has already been taken")
     end
     it "is invalid with a duplicate username" do
-      user = User.new(first_name: 'John', last_name: 'Doe', username: 'johndoe86', email: 'johndoe88@example.com', password: 'password')
-      user.valid?
-      expect(user.errors[:username]).to include("has already been taken")
+      user1 = create(:user, username: 'duplicate')
+      user2 = build(:user, username: 'duplicate')
+      user2.valid?
+      expect(user2.errors[:username]).to include("has already been taken")
     end
   end
 end
