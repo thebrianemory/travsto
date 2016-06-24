5e213d37a5e75a1fe4f8b8836e394fbc079b68ff
thebrianemory <the@brianemory.com>
2016-06-23 22:35:50 -0700

    Add db cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,22 @@ RSpec.configure do |config|
   # Include Factory Girl syntax to simplify calls to factories
   config.include FactoryGirl::Syntax::Methods
 
+  # Configure DatabaseCleaner to reset data between tests
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
