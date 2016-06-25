b8dad8b8066b9a795b3f64b107eb09b231100e64
thebrianemory <the@brianemory.com>
2016-06-24 20:26:47 -0700

    Make business model spec more DRY

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -2,41 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Business, type: :model do
   describe "misc" do
+    before(:each) do
+      @user = build(:user)
+      @category = create(:category)
+      @business = build(:business, category_id: @category.id)
+      @review = build(:review, user_id: @user.id, business_id: @business.id)
+    end
     it "has a valid factory" do
-      expect(build(:business)).to be_valid
+      expect(@business).to be_valid
     end
     it "is valid with a name and category_id" do
-      expect(build(:business)).to be_valid
+      expect(@business).to be_valid
     end
     it "returns a business' average rating" do
-      category = create(:category)
-      business = create(:business, category_id: category.id)
-      user = create(:user)
-      business.reviews << create(:review, business_id: business.id, user_id: user.id)
-      business.reviews << create(:review, rating: 2, business_id: business.id, user_id: user.id)
-      expect(business.average_review_rating).to eq 3
+      @business.reviews << @review
+      @business.reviews << build(:review, user_id: @user.id, business_id: @business.id, rating: 2)
+      @business.save
+      expect(@business.average_review_rating).to eq 3
     end
   end
 
   describe "information cannot be left blank" do
+    before(:each) do
+      @category = create(:category)
+      @business = build(:business, category_id: @category.id)
+    end
     it "is invalid without a name" do
-      business = build(:business, name: nil)
-      business.valid?
-      expect(business.errors[:name]).to include("can't be blank")
+      @business.name = nil
+      @business.valid?
+      expect(@business.errors[:name]).to include("can't be blank")
     end
 
     it "is invalid without a category_id" do
-      business = build(:business, category_id: nil)
-      business.valid?
-      expect(business.errors[:category_id]).to include("can't be blank")
+      @business.category_id = nil
+      @business.valid?
+      expect(@business.errors[:category_id]).to include("can't be blank")
     end
   end
 
   describe "information cannot already be in use" do
+    before(:each) do
+      @category = create(:category)
+      @business1 = create(:business, name: "Weemo's Pizzeria", category_id: @category.id)
+    end
     it "is invalid with a duplicate name" do
-      category = create(:category)
-      business1 = create(:business, name: "Weemo's Pizzeria", category_id: category.id)
-      business2 = build(:business, name: "Weemo's Pizzeria", category_id: category.id)
+      business2 = build(:business, name: "Weemo's Pizzeria", category_id: @category.id)
       business2.valid?
       expect(business2.errors[:name]).to include("has already been taken")
     end
