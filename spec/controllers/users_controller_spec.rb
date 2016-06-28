c33a61aa3ad0326f74946f3fec5181727e8d32ba
thebrianemory <the@brianemory.com>
2016-06-28 09:53:54 -0700

    Add test for user trips action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  before(:each) do
+    @user = create(:user)
+  end
   describe "GET #show" do
-    it 'allows a user to view their profile page' do
-      user = create(:user)
-      get :show, id: user
+    it 'assigns the requested user to @user' do
+      get :show, id: @user
+      expect(assigns(:user)).to render_template :show
+    end
+    it 'renders the :show template' do
+      get :show, id: @user
       expect(response).to render_template :show
     end
   end
@@ -13,6 +19,13 @@ RSpec.describe UsersController, type: :controller do
     login_user
     it "should have a current_user" do
       expect(subject.current_user).to_not eq(nil)
+    end
+  end
+
+  describe 'GET #trips' do
+    it 'allows the user to view their trips' do
+      get :trips, username: @user
+      expect(response).to render_template :trips
     end
   end
 end
