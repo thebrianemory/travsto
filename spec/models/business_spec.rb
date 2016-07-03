98ecf7471b2717cc85059591c4e2b0dd525aea4e
thebrianemory <the@brianemory.com>
2016-07-03 08:47:44 -0700

    Remove unncessary business model tests

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -5,34 +5,16 @@ RSpec.describe Business, type: :model do
     before(:each) do
       @user = create(:user)
       @business = create(:business)
-      @review = create(:review, user_id: @user.id, business_id: @business.id)
-      @category = Category.find_by_id(@business.category_id)
     end
     describe "misc" do
       it "has a valid factory" do
         expect(@business).to be_valid
-      end
-      it "is valid with a name and category_id" do
-        expect(@business).to be_valid
-      end
-      it "returns a business' average rating" do
-        @business.reviews << build(:review, user_id: @user.id, rating: 2)
-        @business.save
-        expect(@business.average_review_rating).to eq 3
       end
     end
 
     describe "associations" do
       before(:each) do
         @trip = create(:trip, user_id: @user.id)
-      end
-      it "belongs to a category" do
-        expect(@business.category).to eq(@category)
-      end
-      it "has many reviews" do
-        @business.reviews << @review
-        @business.save
-        expect(@business.reviews.count).to eq 1
       end
       it "has many trips" do
         @business.trips << @trip
@@ -46,23 +28,6 @@ RSpec.describe Business, type: :model do
     it 'turns the its name into a slug' do
       business = create(:business, name: "Weemo's Pizzeria")
       expect(business.slug).to eq("weemo-s-pizzeria")
-    end
-  end
-
-  describe "information cannot be left blank" do
-    before(:each) do
-      @business = build(:business)
-    end
-    it "is invalid without a name" do
-      @business.name = nil
-      @business.valid?
-      expect(@business.errors[:name]).to include("can't be blank")
-    end
-
-    it "is invalid without a category_id" do
-      @business.category_id = nil
-      @business.valid?
-      expect(@business.errors[:category_id]).to include("can't be blank")
     end
   end
 
