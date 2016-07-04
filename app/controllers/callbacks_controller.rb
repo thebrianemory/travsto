4bf1c5775a929c42e99d447604d54d3d52014c0a
thebrianemory <the@brianemory.com>
2016-07-03 20:09:03 -0700

    Add Facebook rerequest if email not given

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,7 +1,10 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def facebook
+      if request.env["omniauth.auth"].info.email.blank?
+        redirect_to "/auth/facebook?auth_type=rerequest&scope=email"
+      else
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        
         sign_in_and_redirect @user
+      end
     end
 end
